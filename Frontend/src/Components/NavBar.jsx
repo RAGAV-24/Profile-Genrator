99fdad0c5854c84c14cfaaa4344c922d4d33f008
RAGAV-24 <ragavrunner33@gmail.com>
2025-04-28 09:11:33 +0530

    navbar added

---
+++ Frontend/src/Components/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+      <div className="w-4/4 h-15 bg-black">
+      <div className="p-3 flex items-center justify-between">
+        <p className="w-55 p-1 h-10 rounded-xl text-white font-bold hover:text-green-500  hover:bg-white"> GitHub Readme Generator</p>
+
+        <div className="w-18 h-10 rounded-xl text-white hover:bg-white hover:text-black text-center p-2 font-bold">
+        <button  >
+          Login
+        </button>
+        </div>
+
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
